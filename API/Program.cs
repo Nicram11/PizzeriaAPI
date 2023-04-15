5c4c2e35d5a367b16221abdb511a3a7a42bc74ae
nicram11 <mar.roszyk@gmail.com>
2023-04-15 17:08:22 +0200

    add mediatR configuration

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,13 @@
+using Application.Security.Commands;
+using Application.Security.Queries;
+using Domain.Entities;
+using Infrastructure.Data;
+using MediatR;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(LoginUserQuery).Assembly));
+builder.Services.AddDbContext<PizzeriaDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<PizzeriaDbContext>();
+
 
 var app = builder.Build();
 
