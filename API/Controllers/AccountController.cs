43ef89d2ac06358c7552a1b3382d2f0b87cf5f85
nicram11 <mar.roszyk@gmail.com>
2023-04-16 12:59:50 +0200

    Add controllers

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Application.Security.Commands;
+using Application.Security.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private IMediator mediator;
+
+        public AccountController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterUserCommand command)
+        {
+            if (command.Password != command.ConfirmPassword)
+                return BadRequest("The passwords you entered do not match. Please try again");
+
+
+            var result = await mediator.Send(command);
+
+            if (result.Succeeded)
+                return Ok();
+            else
+                return BadRequest(result.Errors);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginUserQuery query)
+        {
+            var result = await mediator.Send(query);
+
+            if (result.Successed)
+                return Ok(result.Token);
+            else
+                return Unauthorized();
+        }
+  
+    }
+}
---
+++ API/Controllers/RestaurantController.cs
@@ -0,0 +1,50 @@
+﻿using Application.Restaurants.Commands;
+using Application.Restaurants.Queries;
+using Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+        public RestaurantController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRestaurant(CreateRestaurantCommand command)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _mediator.Send(command);
+            if (!result.Successed)
+                BadRequest("Failed to create entity");
+
+            return CreatedAtAction(nameof(CreateRestaurant),result.Id);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Restaurant>> GetRestaurantById(int id)
+        {
+            var restaurant = await _mediator.Send(new GetRestaurantByIdQuery { Id = id });
+
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            return restaurant;
+        }
+
+    }
+}
